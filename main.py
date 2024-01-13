dab1618fe9888f383f20e541374ba8c805e2580a
chicoumbetov <shyngysmaaan@gmail.com>
2024-01-13 14:06:22 +0100

    create vars for grid screen

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ pygame.init()
 BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
 YELLOW = (255, 255, 0)
+
+WIDTH, HEIGHT = 800, 800
+TILE_SIZE = 20
+GRID_WIDTH = WIDTH // TILE_SIZE
+GRID_HEIGHT = HEIGHT // TILE_SIZE
+FPS = 60
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+clock = pygame.time.Clock()
